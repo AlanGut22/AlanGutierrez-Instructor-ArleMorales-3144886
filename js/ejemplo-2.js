c4313cf9cff197f413892a59d2280c617ec4ea09
AlanGut22 <162762463+AlanGut22@users.noreply.github.com>
2025-02-20 08:57:53 -0500

    Delete js directory

--- js/ejemplo-2.js
+++
@@ -1,4 +0,0 @@
-const usuario = "12345";
-const contraseña = "12345*";
-
-usuario == "12345" && contraseña == "12345*" ? console.log(`El usuario es un empleado`) : console.log(`El usuario no es un empleado`)
--- js/prueba.js
+++
@@ -1,8 +0,0 @@
-let color = "rojo"
-let estadoSemaforo = true
-
-let mensajeSemaforo = color == "verde"? `seguir..` : color == "rojo"? `detengase`:
-color == "amarillo"? `alistarse` : `dañado`
-
-let encendido = estadoSemaforo? mensajeSemaforo : `Esta apagado`
-console.log(encendido)
