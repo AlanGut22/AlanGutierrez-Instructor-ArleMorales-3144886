71c76e0f56b474c2554f4aa1064d30b75a17c6e2
AlanGut22 <162762463+AlanGut22@users.noreply.github.com>
2025-03-18 09:51:10 -0500

    Add files via upload

---
+++ ejercicios - Map, Set, Arrays/Ejercicio3.js
@@ -0,0 +1,6 @@
+const asistentes = new Map([[1, "Ana"], [2, "Luis"]]);
+const agregarAsistente = (map, id, nombre) => {
+    map.set(id, nombre);
+}
+agregarAsistente(asistentes, 3, "Sofia");
+console.log(asistentes)
