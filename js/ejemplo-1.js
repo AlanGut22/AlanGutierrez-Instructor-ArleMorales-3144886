d1748b45b6651e33429fc220bac56f6b141d1746
AlanGut22 <162762463+AlanGut22@users.noreply.github.com>
2025-04-23 18:51:03 -0500

    Delete js directory

--- js/ejemplo-1.js
+++
@@ -1,6 +0,0 @@
-let ocupacionCcomercial = true
-let nomCcomercial = "Unicentro"
-
-mensaje = ocupacionCcomercial ? `El centro comercial ${nomCcomercial} esta ocupado` : `El centro comercial ${nomCcomercial} esta vacio`
-
-console.log(mensaje)
